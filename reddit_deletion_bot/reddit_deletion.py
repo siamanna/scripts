e03d623bcdf3b5891c4eb7d3e8679225622181fe
siamanna <Sia.Manna@unh.edu>
2024-06-26 22:55:24 -0400

    ADD reddit deletion bot

---
+++ reddit_deletion_bot/reddit_deletion.py
@@ -0,0 +1,8 @@
+def display_delete_comments_andor_posts():
+    print("Select what user content you would like to delete: ")
+    print("1. DELETE COMMENTS")
+    print("2. DELETE POSTS")
+    print("3. DELETE BOTH")
+
+def get_user_input():
+    user_input = input("What posts would you like to delete")
