880a53b092a387e8395166c8e8fb57e8036762d2
siamanna <Sia.Manna@unh.edu>
2024-06-26 22:47:29 -0400

    worked on unfollower bot tutorial- need to make similar script

--- unfollower_spy_bot.py
+++ unfollower_spy_bot.py
@@ -5,11 +5,22 @@ import time
 username = "username"
 password = "password"
 
+def login(driver):
+    driver.find_element_by_name("username").send_keys(username)
+    driver.find_element_by_name("password").send_keys(password)
+    driver.find_element_by_name("password").send_keys(u'\ue007')
+
 def __main__():
 
     driver = webdriver.Chrome(ChromeDriverManager.install())
     driver.get('https://www.instagram.com/accounts/login/?source=auth_switcher')
-    time.sleep(10)
+    time.sleep(1)
+    login()
+
+    
+    time.sleep(10000)
+
+
     return
 
 __main___()
